d9628965c7dd0d5c5c740b0685ce0fcc41ffeeb8
Ryan Gieg <ryangieg@sbcglobal.net>
2024-08-21 13:10:51 -0700

    Implemented detection system for AI entities

--- Assets/Scripts/AI/AIDetector.cs
+++ Assets/Scripts/AI/AIDetector.cs
@@ -4,15 +4,80 @@ using UnityEngine;
 
 public class AIDetector : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Range(1, 15)]
+    [SerializeField]
+    private float viewRadius = 11;
+    [SerializeField]
+    private float detectionCheckDelay = 0.1f;
+    [SerializeField]
+    private Transform target = null;
+    [SerializeField]
+    private LayerMask playerLayerMask;
+    [SerializeField]
+    private LayerMask visibilityLayer;
+
+    [field: SerializeField]
+    public bool TargetVisible { get; private set; }
+
+    public Transform Target
     {
-        
+        get => target;
+        set {
+            target = value;
+            TargetVisible = false;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        StartCoroutine(DetectionCoroutine());
+    }
+
+    private void Update()
+    {
+        if (Target != null)
+            TargetVisible = CheckTargetVisible();
+    }
+
+    private bool CheckTargetVisible()
+    {
+        var result = Physics2D.Raycast(transform.position, Target.position - transform.position, viewRadius, visibilityLayer);
+        if (result.collider != null)
+            return (playerLayerMask & (1 << result.collider.gameObject.layer)) != 0;
+        return false;
+    }
+
+    private void DetectTarget()
+    {
+        if (Target == null)
+            CheckIfPlayerInRange();
+        else if (Target != null)
+            DetectIfOutOfRange();
+    }
+
+    private void CheckIfPlayerInRange()
+    {
+        Collider2D collision = Physics2D.OverlapCircle(transform.position, viewRadius, playerLayerMask);
+        if (collision != null)
+            Target = collision.transform;
+    }
+
+    private void DetectIfOutOfRange()
+    {
+        if (Target == null || Target.gameObject.activeSelf == false || 
+            Vector2.Distance(transform.position, Target.position) > viewRadius)
+            Target = null;
+    }
+
+    IEnumerator DetectionCoroutine()
+    {
+        yield return new WaitForSeconds(detectionCheckDelay);
+        DetectTarget();
+        StartCoroutine(DetectionCoroutine());
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, viewRadius);
     }
 }
