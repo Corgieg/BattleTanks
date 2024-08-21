6e9bc90753ca30fd61bebb9b439f55f8ce038f1e
Ryan Gieg <ryangieg@sbcglobal.net>
2024-08-21 16:31:22 -0700

    Began implementing AI behaviors

---
+++ Assets/Scripts/AI/AIBehaviour.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class AIBehaviour : MonoBehaviour
+{
+    public abstract void PerformAction(TankController tank, AIDetector detector);
+}
